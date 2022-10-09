fe0fb0b6cd0880505cceedc280626cc41b6685b8
LeeYongwoo-kor <newlyo1205@naver.com>
2022-10-10 02:14:48 +0900

    Add login model of prisma/schema, useMutation, withHandler

---
+++ pages/libs/client/useMutation.ts
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+interface UseMutationState<T> {
+  data?: T;
+  error?: object;
+}
+
+export default function useMutation<T>(url: string) {
+  const [state, setState] = useState<UseMutationState<T>>({
+    data: undefined,
+    error: undefined,
+  });
+  const mutation = (data: any) => {
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json().catch(() => {}))
+      .then((data) => setState((prev) => ({ ...prev, data })))
+      .catch((error) => setState((prev) => ({ ...prev, error })));
+  };
+
+  return [mutation, { ...state }];
+}
---
+++ pages/libs/server/withHandler.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+type method = "GET" | "POST" | "DELETE";
+
+interface ConfigType {
+  methods: method[];
+  handler: (req: NextApiRequest, res: NextApiResponse) => void;
+  isPrivate?: boolean;
+}
+
+export default function withHandler({
+  methods,
+  handler,
+  isPrivate = true,
+}: ConfigType) {
+  return async function (
+    req: NextApiRequest,
+    res: NextApiResponse
+  ): Promise<any> {
+    if (req.method && !methods.includes(req.method as any)) {
+      return res.status(405).end();
+    }
+    if (isPrivate && !req.session.user) {
+      return res.status(401).json({ ok: false });
+    }
+    try {
+      handler(req, res);
+    } catch (error) {
+      return res.status(500).json({ error });
+    }
+  };
+}
